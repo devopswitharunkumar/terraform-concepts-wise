922a4a14c77a1073a90474edaeb23b65fb281448
arunkumar261 <arunkumar261@gmail.com>
2026-06-28 20:57:53 +0530

    updated syntax example

--- for_each-with-ec2-route53/ec2.tf
+++ for_each-with-ec2-route53/ec2.tf
@@ -18,3 +18,7 @@ resource "aws_route53_record" "route53" {
   ttl     = 1
   records = [each.key == "web" ? aws_instance.ec2[each.key].public_ip : aws_instance.ec2[each.key].private_ip]
 }
+
+#above method or below method we can use
+#for_each = aws_instance.ec2
+#records = [each.key == "web" ? each.value.public_ip : each.value.private_ip]
